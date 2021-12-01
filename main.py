db4d224d1cca58bdc05772c1aea8abc9f265627a
william <willchung4@gmail.com>
2021-12-01 13:09:07 -0500

    removed gif functionality; save collage

--- main.py
+++ main.py
@@ -18,16 +18,15 @@ import random
 FONT_NAME = 'Courier'
 HEIGHT = 2160
 WIDTH = 3840
-
 BG_COLOR = '#89CFF0'
 
-main_canvas = Image.new(mode='RGBA', color=BG_COLOR, size=(WIDTH, HEIGHT))
-gif_imgs = []
+# main_canvas = Image.new(mode='RGBA', color=BG_COLOR, size=(WIDTH, HEIGHT))
 
 # ---------------------------- Iffy Fishy Functions ------------------------------- #
 def create_fish():
     global orig_img_to_save
     global trans_img_to_save
+    global resized_img_to_save
 
     size_multiplier = calc_scale_multiplier(random.randrange(1, 751, 5))
     fish_funcs = [draw_fish_design_1(), draw_fish_design_2(), draw_fish_design_3()]
@@ -39,18 +38,21 @@ def create_fish():
     transformed_fish_img = transform(original_fish_img, rotation_range)
     resized_transformed_fish_img = resize(transformed_fish_img, size_multiplier)
 
-    # # add to global fish images list
-    # fish_imgs.append((original_fish_img, resized_transformed_fish_img))
-
     # save original image and transformed image
     orig_img_to_save = original_fish_img
     trans_img_to_save = transformed_fish_img
+    resized_img_to_save = resized_transformed_fish_img
     
     return resized_transformed_fish_img
 
+
 def display_fish(fish):
+    '''Resized preview canvas and displays fish'''
     global orig_img_to_save
     global trans_img_to_save    
+
+    # resize canvas for single fish
+    preview_canvas.config(width=500, height=500)
 
     # resize to fit 500x500 canvas
     fish_img_width, fish_img_height = fish.size
@@ -58,11 +60,27 @@ def display_fish(fish):
 
     fish = ImageTk.PhotoImage(resized_fish_img)
     root.fish = fish # prevent the image garbage collected
-    preview_canvas.create_image(250, 250, image=fish, anchor='center')
+    preview_canvas.itemconfig(canvas_image, image=fish)
+
 
 def create_and_display():
+    '''Driver function for creating and displaying fish'''
+    global canvas_image
+
+    # Enable image buttons; Disable collage buttons
+    save_orig_btn.config(state=tk.NORMAL)
+    save_trans_btn.config(state=tk.NORMAL)
+    save_resized_btn.config(state=tk.NORMAL)
+    collage_add_btn.config(state=tk.DISABLED)
+    collage_save_btn.config(state=tk.DISABLED)
+
+    preview_canvas.delete('all')
+    preview_canvas.config(width=500, height=500)
+    canvas_image = preview_canvas.create_image(250, 250, image=None, anchor='center')
+
     fish = create_fish()
     display_fish(fish)
+
 
 def save_orig_fish():
     if orig_img_to_save is None:
@@ -75,6 +93,7 @@ def save_orig_fish():
 
     orig_img_to_save.save(filename)
 
+
 def save_trans_fish():
     if trans_img_to_save is None:
         return
@@ -86,64 +105,105 @@ def save_trans_fish():
 
     trans_img_to_save.save(filename)
 
-def ready_canvas_for_gif():
-    '''Clears the canvas and sets dimensions for GIF'''
-    global gif_image
+
+def save_resized_fish():
+    if resized_img_to_save is None:
+        return
+    
+    filename = filedialog.asksaveasfile(mode='wb', title='Save file', initialfile='your_iffy_fishy_resized', defaultextension='.png', filetypes=(('PNG', ('*.jpg','*.jpeg','*.jpe','*.jfif')),('PNG', '*.png'),('BMP', ('*.bmp','*.jdib')),('GIF', '*.gif')))
+
+    if not filename:
+        return
+
+    resized_img_to_save.save(filename)
+
+
+def create_fishtopia(**kwargs):
+    '''Create fishtopia collage with amount of fish, default = 50'''
+    global fishtopia_collage
+
+    # Disable image buttons; Enable collage buttons
+    save_orig_btn.config(state=tk.DISABLED)
+    save_trans_btn.config(state=tk.DISABLED)
+    save_resized_btn.config(state=tk.DISABLED)
+    collage_add_btn.config(state=tk.NORMAL)
+    collage_save_btn.config(state=tk.NORMAL)
+
+    fishtopia_collage = Image.new('RGBA', (WIDTH, HEIGHT), color=BG_COLOR)
+
+    if 'amount' in kwargs:
+        try:
+            amount = int(kwargs['amount'])
+        except ValueError:
+            pass
+    else:
+        amount = 50
+
+    for _ in range(amount):
+        fish = create_fish()
+        
+        cors = (random.randrange(5, 3700), random.randrange(5, 2000))
+        fishtopia_collage.alpha_composite(fish, dest=cors)
+
+    preview = ImageTk.PhotoImage(fishtopia_collage.resize((960, 540), Image.ANTIALIAS))
+    root.preview = preview # save image data to local variable to bypass bug with photoimage
+    preview_canvas.itemconfig(canvas_image, image=preview)
+
+
+def create_collage():
+    '''Driver function for create_fishtopia'''
+    global canvas_image
 
     preview_canvas.delete('all')
     preview_canvas.config(width=960, height=540)
-    gif_preview = gif_image.resize((960, 540), Image.ANTIALIAS)
-    ph_gif_preview = ImageTk.PhotoImage(gif_preview)
-    root.ph_gif_preview = ph_gif_preview
-    preview_canvas.itemconfig(canvas_image, image=ph_gif_preview)
+    canvas_image = preview_canvas.create_image(0, 0, image=None, anchor='nw')
 
-def create_gif(frames):
-    '''Creates iffy fishy images and returns a GIF '''
-    global frame_counter
-    global gif_frames
-    global gif_image
-
-    gif_preview = gif_image.resize((960, 540), Image.ANTIALIAS)
-    ph_gif_preview = ImageTk.PhotoImage(gif_preview)
-    root.ph_gif_preview = ph_gif_preview
-    canvas_image = preview_canvas.create_image(480, 270, image=ph_gif_preview, anchor='center')
-    
-    image = create_fish()
-
-    cors = (random.randrange(25, 3500), random.randrange(25, 1900))
-    gif_image.alpha_composite(image, dest=cors)
-    gif_preview = gif_image.resize((960, 540), Image.ANTIALIAS)
-    ph_gif_preview = ImageTk.PhotoImage(gif_preview)
-    root.ph_gif_preview = ph_gif_preview
-
-    preview_canvas.itemconfig(canvas_image, image=ph_gif_preview)
-
-    gif_frames.append(gif_image.resize((1920, 1080), Image.ANTIALIAS))
-
-    if frames > 1:
-        root.after(25, create_gif, frames-1)
-
-def create_50():
-    ready_canvas_for_gif()
-    create_gif(50)
-
-def add_to_gif():
     try:
-        num = num_fishies.get()
-        create_gif(int(num))
-        num_entry.delete(0, len(num))
+        num_fishies = int(starting_fishies.get())
+        create_fishtopia(amount=num_fishies)
+    except ValueError:
+        create_fishtopia()
+    finally:
+        starting_fishies_entry.delete(0, tk.END)
+    
+
+def add_to_fishtopia():
+    '''Adds specified number of fish to fishtopia collage'''
+    global fishtopia_collage
+
+    try:
+        num_fishies = int(num_fishies_to_add.get())
     except ValueError:
         return
+    
+    for _ in range(num_fishies):
+        fish = create_fish()
 
-def save_gif():
-    global gif_frames
-    global gif_image
+        cors = (random.randrange(25, 3500), random.randrange(25, 1900))
+        fishtopia_collage.alpha_composite(fish, dest=cors)
 
-    filename = filedialog.asksaveasfile(mode='wb', title='Save file', initialfile='your_iffy_fishy_giffy', defaultextension='.gif', filetypes=[('GIF', '*.gif')])
-    gif_frames[0].save(filename, save_all=True, append_images=gif_frames[1:], optimize=True, duration=125, loop=0)
+    preview = ImageTk.PhotoImage(fishtopia_collage.resize((960, 540), Image.ANTIALIAS))
+    root.preview = preview # save image data to local variable to bypass bug with photoimage
+    preview_canvas.itemconfig(canvas_image, image=preview)
 
-    # reset gif_image
-    gif_image = Image.new(mode='RGBA', color=BG_COLOR, size=(WIDTH, HEIGHT))
+    # clear entry text
+    add_fishies_entry.delete(0, tk.END)
+
+
+def save_fishtopia():
+    '''Saves final canvas as an image'''
+    global fishtopia_collage
+
+    if fishtopia_collage is None:
+        return
+
+    filename = filedialog.asksaveasfile(mode='wb', title='Save file', initialfile='your_iffy_fishy_fishtopia', defaultextension='.gif', filetypes=(('PNG', ('PNG', '*.png')),('BMP', ('*.bmp','*.jdib')),('GIF', '*.gif')))
+
+    if not filename:
+        return
+
+    fishtopia_collage.save(filename)
+
 
 # ---------------------------- main ------------------------------- #
 
@@ -154,70 +214,68 @@ if __name__=='__main__':
 
     logo_img = Image.open('./images/logo.png')
     logo_width, logo_height = logo_img.size
-    resized_logo = logo_img.resize((logo_width//2, logo_height//2), resample=Image.ANTIALIAS)
-    logo = ImageTk.PhotoImage(resized_logo)
+    logo = ImageTk.PhotoImage(logo_img.resize((round(logo_width/1.5), round(logo_height/1.5)), resample=Image.ANTIALIAS))
     logo_label = tk.Label(image=logo)
     logo_label.grid(column=0, row=0)
 
     # Initialize preview canvas with default screen
     preview_canvas = tk.Canvas(width=500, height=500)
     preview_canvas.grid(column=0, row=1)
-    default_preview = ImageTk.PhotoImage(Image.new(mode='RGBA', color=BG_COLOR, size=(500, 500)))
-    canvas_image = preview_canvas.create_image(250, 250, image=default_preview, anchor='center')
+    canvas_image = preview_canvas.create_image(250, 250, image=None, anchor='center')
     
+    # Global variables
     orig_img_to_save = None
     trans_img_to_save = None
-
-    fish_imgs = [] # (orig, trans)
-
-    # GIF variables
-    gif_frames = []
-
-    gif_image = Image.new(mode='RGBA', color=BG_COLOR, size=(WIDTH, HEIGHT))
+    resized_img_to_save = None
+    fishtopia_collage = None
 
     # Button frame
     button_frame = tk.Frame(root)
     button_frame.grid(column=0, row=2)
     
-    # Image button frame
+    # Frame for single fish image related functions
     img_button_frame = tk.Frame(button_frame)
     img_button_frame.grid(column=0, row=0, padx=(0, 75))
 
     fish_btn = tk.Button(img_button_frame, text='Create Fish', font=(FONT_NAME, 11, 'normal'), command=create_and_display)
-    fish_btn.grid(column=0, row=1, padx=5, pady=5, sticky='nesw')
+    fish_btn.grid(column=0, row=0, padx=5, pady=5, sticky='nesw')
 
-    save_orig_btn = tk.Button(img_button_frame, text='Save Original Fish', font=(FONT_NAME, 11, 'normal'), command=save_orig_fish)
-    save_orig_btn.grid(column=0, row=2, padx=5, pady=5, sticky='nesw')
+    save_orig_btn = tk.Button(img_button_frame, text='Save Original', font=(FONT_NAME, 11, 'normal'), command=save_orig_fish, state=tk.DISABLED)
+    save_orig_btn.grid(column=0, row=1, padx=5, pady=5, sticky='nesw')
 
-    save_trans_btn = tk.Button(img_button_frame, text='Save Transformed Fish', font=(FONT_NAME, 11, 'normal'), command=save_trans_fish)
-    save_trans_btn.grid(column=0, row=3, padx=5, pady=5, sticky='nesw')
+    save_trans_btn = tk.Button(img_button_frame, text='Save Transformed', font=(FONT_NAME, 11, 'normal'), command=save_trans_fish, state=tk.DISABLED)
+    save_trans_btn.grid(column=0, row=2, padx=5, pady=5, sticky='nesw')
 
-    # Gif button frame
-    gif_button_frame  = tk.Frame(button_frame)
-    gif_button_frame.grid(column=1, row=0)
+    save_resized_btn = tk.Button(img_button_frame, text='Save Resized', font=(FONT_NAME, 11, 'normal'), command=save_resized_fish, state=tk.DISABLED)
+    save_resized_btn.grid(column=0, row=3, padx=5, pady=5, sticky='nesw')
 
-    gif_create_50_btn = tk.Button(gif_button_frame, text='Create 50', font=(FONT_NAME, 11, 'normal'), command=create_50)
-    gif_create_50_btn.grid(column=0, row=0, padx=5, pady=5, sticky='nesw')
+    # Frame for Fishtopia Collage buttons
+    collage_btns_frame  = tk.Frame(button_frame)
+    collage_btns_frame.grid(column=1, row=0)
 
-    # Gif add frame
-    gif_add_frame = tk.Frame(gif_button_frame)
-    gif_add_frame.grid(column=0, row=1)
+    # Create fishtopia frame
+    collage_create_btns_frame = tk.Frame(collage_btns_frame)
+    collage_create_btns_frame.grid(column=0, row=0)
 
-    gif_add_btn = tk.Button(gif_add_frame, text='Add', font=(FONT_NAME, 11, 'normal'), command=add_to_gif)
-    gif_add_btn.grid(column=0, row=0, padx=5, pady=5, sticky='nesw')
+    collage_create_btn = tk.Button(collage_create_btns_frame, text='Create Fishtopia', font=(FONT_NAME, 11, 'normal'), command=create_collage)
+    collage_create_btn.grid(column=0, row=0, padx=5, pady=5, sticky='nesw')
 
-    num_fishies = tk.StringVar()
-    num_entry = tk.Entry(gif_add_frame, textvariable=num_fishies, bd=5)
-    num_entry.grid(column=1, row=0, sticky='nesw')
+    starting_fishies = tk.StringVar()
+    starting_fishies_entry = tk.Entry(collage_create_btns_frame, textvariable=starting_fishies, bd=5, width=4)
+    starting_fishies_entry.grid(column=1, row=0, sticky='nesw')
 
-    save_fishtopia_btn = tk.Button(gif_button_frame, text='Save Fishtopia', font=(FONT_NAME, 11, 'normal'), command=None)
-    save_fishtopia_btn.grid(column=0, row=2, padx=5, pady=5, sticky='nesw')
+    # Add fishies frame
+    collage_add_btn_frame = tk.Frame(collage_btns_frame)
+    collage_add_btn_frame.grid(column=0, row=1, sticky='nesw')
 
-    gif_save_btn = tk.Button(gif_button_frame, text='Save GIF', font=(FONT_NAME, 11, 'normal'), command=save_gif)
-    gif_save_btn.grid(column=0, row=3, padx=5, pady=5, sticky='nesw')
+    collage_add_btn = tk.Button(collage_add_btn_frame, text='Add Fishies', font=(FONT_NAME, 11, 'normal'), command=add_to_fishtopia, state=tk.DISABLED)
+    collage_add_btn.grid(column=0, row=0, padx=5, pady=5, sticky='nesw')
+
+    num_fishies_to_add = tk.StringVar()
+    add_fishies_entry = tk.Entry(collage_add_btn_frame, textvariable=num_fishies_to_add, bd=5, width=11)
+    add_fishies_entry.grid(column=1, row=0, sticky='nesw')
+
+    collage_save_btn = tk.Button(collage_btns_frame, text='Save Fishtopia', font=(FONT_NAME, 11, 'normal'), command=save_fishtopia, state=tk.DISABLED)
+    collage_save_btn.grid(column=0, row=2, padx=5, pady=5, sticky='nesw')
 
     root.mainloop()
-
-
-
-    
